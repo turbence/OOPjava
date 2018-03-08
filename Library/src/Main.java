5fccfd482f08b3b54777417313d37494c04da614
Turcsányi Bence <Turcsnyi Bence@192.168.2.126>
2018-03-08 13:26:43 +0100

    Library

---
+++ Library/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+	public static void main(String[] args) {
+		Book book = new Book();
+		book.setAuthor("J.K. Rowling");
+		book.setPrice(2);
+		book.setReleaseYear(2008);
+		book.setTitle("Harry Potter");
+		
+
+	}
+}
